0ef07b78c74aab1acc583e1b6982f973b42082b4
Martin Zenzes (spacelab1-u) <mzenzes@mega2000.de>
2010-06-18 13:20:59 +0200

    suchscript for new commandline parameters addapted
    Signed-off-by: Martin Zenzes (spacelab1-u) <mzenzes@mega2000.de>

--- build/searchAll_Baud_Id.sh
+++ build/searchAll_Baud_Id.sh
@@ -4,7 +4,7 @@ for baud in 9600 19200 38400 57600 115200 230400; do
 	echo $baud
 	for id in `seq 0 1 253`; do
 		#echo $baud $id
-		./dynamixel_test /dev/ttyUSB0 $id $baud &> search.log
+		./dynamixel_test -port /dev/ttyUSB0 -id $id -baud $baud -connect &> search.log
 		if [ $? == 0 ];then
 			echo year! $baud $id
 		fi
